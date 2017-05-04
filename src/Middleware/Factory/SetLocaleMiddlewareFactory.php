4c81634e19941103b55a4af68f1d39ced844616c
Michael Jochum <michael.jochum@jochum-mediaservices.de>
2017-05-04 16:10:40 +0200

    get locale from request or set a standard, prepare first application and page parameters in PageParameters

--- src/Middleware/Factory/SetLocaleMiddlewareFactory.php
+++ src/Middleware/Factory/SetLocaleMiddlewareFactory.php
@@ -7,9 +7,18 @@ use Zend\Expressive\Helper\UrlHelper;
 
 class SetLocaleMiddlewareFactory
 {
+    private $locales = array(
+        'de' => 'de_DE.utf8',
+        'gb' => 'en_GB.utf8',
+        'it' => 'it_IT.utf8',
+        'ca' => 'fr_CA.utf8',
+        'es' => 'es_ES.utf8',
+        'mx' => 'es_MX.utf8',
+        'us' => 'en_US.utf8',
+    );
 
     public function __invoke(ContainerInterface $container)
     {
-        return new SetLocaleMiddleware($container->get(UrlHelper::class));
+        return new SetLocaleMiddleware($container->get(UrlHelper::class), $this->locales);
     }
 }
--- src/Middleware/I18n/SetLocaleMiddleware.php
+++ src/Middleware/I18n/SetLocaleMiddleware.php
@@ -6,19 +6,23 @@ use Interop\Http\ServerMiddleware\MiddlewareInterface;
 use Locale;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Expressive\Helper\UrlHelper;
+use ContentinumComponents\Options\PageParameters;
 
 class SetLocaleMiddleware implements MiddlewareInterface
 {
 
     private $helper;
     
+    private $locales = array();
+    
     /**
      * 
      * @param UrlHelper $helper
      */
-    public function __construct(UrlHelper $helper)
+    public function __construct(UrlHelper $helper, $locales)
     {
         $this->helper = $helper;
+        $this->locales = $locales;
     }
     
     /**
@@ -31,13 +35,27 @@ class SetLocaleMiddleware implements MiddlewareInterface
         $uri = $request->getUri();
         $path = $uri->getPath();
         
+        // set page parameters: scheme, host, port and path
+        $pageOptions = PageParameters::getInstance();
+        $pageOptions->setHost($uri->getHost());
+        $pageOptions->setProtocol($uri->getScheme());
+        $pageOptions->setPort($uri->getPort());
+        
         if (! preg_match('#^/(?P<locale>[a-z]{2,3}([-_][a-zA-Z]{2}|))/#', $path, $matches)) {
-            Locale::setDefault('de_DE');
+            Locale::setDefault($this->locales['de']);
+            $pageOptions->setLocale($this->locales['de']);
+            $pageOptions->setQuery($path);
+            $pageOptions->generateParams();
             return $delegate->process($request);
         }
         
         $locale = $matches['locale'];
-        Locale::setDefault(Locale::canonicalize($locale));
+        $default = (isset($this->locales[$locale])) ? $this->locales[$locale] : $this->locales['de'];
+        Locale::setDefault($default);
+        $pageOptions->setLocale($default);
+        $path = str_replace($matches[0] , '', $path);
+        $pageOptions->setQuery($path);
+        $pageOptions->generateParams();
         $this->helper->setBasePath($locale);
         
         return $delegate->process($request->withUri($uri->withPath(substr($path,3))));
