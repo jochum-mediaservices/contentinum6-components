effffa4d2977ecfd9d2133eafa226570f55db164
Michael Jochum <michael.jochum@jochum-mediaservices.de>
2017-05-04 16:11:56 +0200

    move page parameter settings in SetLocaleMiddleware

--- src/Middleware/Pages/SetPageOptionsMiddleware.php
+++ src/Middleware/Pages/SetPageOptionsMiddleware.php
@@ -3,16 +3,15 @@ namespace ContentinumComponents\Middleware\Pages;
 
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
-use Locale;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Expressive\Helper\UrlHelper;
-use ContentinumComponents\Options\PageOptions;
-use ContentinumComponents\Options\PageParameters;
 
 class SetPageOptionsMiddleware implements MiddlewareInterface
 {
 
     private $helper;
+    
+
     
     /**
      * 
@@ -27,31 +26,9 @@ class SetPageOptionsMiddleware implements MiddlewareInterface
      * 
      * {@inheritDoc}
      * @see \Interop\Http\ServerMiddleware\MiddlewareInterface::process()
-     * @return \Contentinum\Options\PageOptions
      */
     public function process(ServerRequestInterface  $request,DelegateInterface $delegate)
     {
-        $uri = $request->getUri();
-        $path = $uri->getPath();
-        
-        $pageOptions = PageParameters::getInstance();
-        $pageOptions->setHost($uri->getHost());
-        $pageOptions->setProtocol($uri->getScheme());
-        $pageOptions->setPort($uri->getPort());
-        
-        
-        if (! preg_match('#^/(?P<locale>[a-z]{2,3}([-_][a-zA-Z]{2}|))/#', $path, $matches)) {
-            $pageOptions->setLocale(Locale::getDefault());
-            $pageOptions->setQuery($path);
-            $pageOptions->generateParams();
-            return $delegate->process($request);
-        }
-        
-        $locale = $matches['locale'];
-        $pageOptions->setLocale(Locale::canonicalize($locale));
-        $path = str_replace($matches[0] , '', $path);
-        $pageOptions->setQuery($path);
-        $pageOptions->generateParams();
         
         return $delegate->process($request);
     }
